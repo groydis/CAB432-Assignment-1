5b2e56a7c8d276174f0a6212ad0b12e86eba354f
Greyden Scott <greyden@Greydens-MacBook-Pro.local>
2018-09-10 16:24:59 +1000

    Code Clean Up
    Commented code and renamed variables so that they more reflect the tone of the assignment.
    
    Made Tone Handling Async to better handle errors
    
    Tweaked the overall design of the site.

--- BuzzTone/routes/index.js
+++ BuzzTone/routes/index.js
@@ -9,11 +9,15 @@ const newsapi = new NewsAPI('bf12e04bc8c446c0862f63962dff0bad');
 
 
 router.get('/', function(req, res, next) {
-	const query = 'OMG'
+	const title = 'Top Articles from BuzzFeed'
+	// Retrieve the top headlines from Buzzfeed
 	processor.getTopArticles().then((response) => {
-		let headlines = processor.buildHeadlines(response);
-		processor.getTones(headlines, function(tone_headlines, doc_tones){
-			res.render('index', { title: query, headlines: tone_headlines, document_tones: doc_tones });
+		// Filter the responses and create objects to be displayed
+		let articles = processor.buildArticles(response);
+		// Apply tones to the objects
+		processor.getTones(articles, function(toned_articles, doc_tones){
+			// Render the page
+			res.render('index', { title: title, articles: toned_articles, document_tones: doc_tones });
 		});
 	}).catch((err) => {
 	  throw err;
@@ -21,12 +25,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+	// Grab the query from the POST request
 	let query = req.body.query;
+	if (query == '') {
+		res.render('error', { message: "No query entered, please use a query. eg 'Trump'", error: query});
+	}
+	let title = 'Articles from BuzzFeed about: ' + query;
+	// Search Articles based on the query
 	processor.getAllArticles(query).then(response => {
-		let headlines = processor.buildHeadlines(response);
-		processor.getTones(headlines, function(tone_headlines, doc_tones){
-			res.render('index', { title: query, headlines: tone_headlines, document_tones: doc_tones });
+		// Filter the response and create objects to be displayed
+		let articles = processor.buildArticles(response);
+		// Apply tones to the objects
+		processor.getTones(articles, function(toned_articles, doc_tones){
+			// Render the page
+			res.render('index', { title: title, articles: toned_articles, document_tones: doc_tones });
+		})
+		.catch((err) => {
+			res.render('error', { message: 'An error occured retriving tones', error: err });
 		});
+	})
+	.catch((err) => {
+		res.render('error', { message: 'An error occured retriving articles', error: err });
 	});
 });
 
--- BuzzTone/scripts/processes.js
+++ BuzzTone/scripts/processes.js
@@ -1,5 +1,3 @@
-const promise = require('promise');
-
 const NewsAPI = require('newsapi');
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
@@ -11,101 +9,132 @@ const toneAnalyzer = new ToneAnalyzerV3({
     url: 'https://gateway-syd.watsonplatform.net/tone-analyzer/api'
 });
 
+// Retrieves top articles
 async function getTopArticles() {
-	return new Promise(resolve => {
+	// Create a promise
+	return new Promise((resolve, reject) => {
+		// Call the News API query for top headlines
 		newsapi.v2.topHeadlines({
+			// Specify the Source
 			sources: 'buzzfeed',
+			// Request 50 queries
 			pageSize: 50,
 		}).then(response => {
+			// Check that the results are all g
 			if (response.status === 'ok') {
-				console.log(response.totalResults);
+				// Shoot the response as the resolve for the promise
 				resolve(response);
 			} else {
-				console.log("Fetching Articles: An Error Occured!")
+				// Return an error
+				reject("Fetching Articles: An Error Occured!")
 			}
 		});
 	});
 }
 
+// Retrives articles bacsed on query supplied
 async function getAllArticles(query) {
-	return new Promise(resolve => {
+	// Create a promise
+	return new Promise((resolve, reject) => {
+		// Call teh News API query to retrieve articles based on query
 		newsapi.v2.everything({
+			// Pass the query to the paramters of the API
 			q: query,
+			// Specify the source
 			sources: 'buzzfeed',
+			// Better make sure the results are in english
 			language: 'en',
+			// Lets sort by popularity
 			sortBy: 'popularity',
+			// Request 50 queries
 			pageSize: 50,
 		}).then(response => {
+			// Check that teh results are all g
 			if (response.status === 'ok') {
-				console.log(response.totalResults);
+				// Shoot the response as the resolve for the promise
 				resolve(response);
 			} else {
-				console.log("Fetching Articles: An Error Occured!")
+				// Return an error
+				reject("Fetching Articles: An Error Occured!");
 			}
 		});
 	});
 }
 
-function buildHeadlines(response) {
-	let headlines = [];
+// Cleaning the responses
+function buildArticles(response) {
+	// Craete an empty array to store the article objects once they are crated
+	let articles = [];
+	// looop through the articles in the response
 	for (let i = 0; i < response.articles.length; i++) {
-		let article = response.articles[i];
-		let headline = {
-			source: article.source.name,
-			title: article.title,
-			description: article.description.replace('View Entire Post ›', ''),
-			url: article.url,
-			image: article.urlToImage,
+		// Store each response in a temporary variable to make code look cleanr (you're welcome)
+		let response_item = response.articles[i];
+		// Create an object
+		let article = {
+			// Store the source
+			//source: response_item.source.name,
+			// Store the title
+			title: response_item.title,
+			// Store the description, do a cheeky little clean to remove some excess
+			//info sometimes placed on the end of the descriptions
+			description: response_item.description.replace('View Entire Post ›', ''),
+			// Store the article URL
+			url: response_item.url,
+			// Grab the associated image
+			image: response_item.urlToImage,
+			// Create a unique div element to display the chart in
 			div: 'chart' + i,
 		}
-		headlines[i] = headline;
+		// Stash the object into the array
+		articles[i] = article;
 	}
-	return headlines;
+	// Return the created array full of article objects
+	return articles;
 }
 
-function getTones(headlines, callback) {
-	let headline_descriptions = '';
+function getTones(articles, callback) {
+	return new Promise((resolve, reject) => {
+		let article_descriptions = '';
 
-	for (let i = 0; i < headlines.length; i++) {
+		for (let i = 0; i < articles.length; i++) {
 
-		let title = headlines[i].description;
-		let text = title.replace(/\./g,'');
-		text = text.replace(/\-/g,'');
-		text = text.replace(/\>/g,'');
-		text = text.replace(/BuzzFeed News/g, '');
-		headline_descriptions += text + '. ';
-	}
-
-	var toneParams = {
-			'tone_input': { 'text': headline_descriptions },
-			'content_type': 'application/json',
-	};
-
-	toneAnalyzer.tone(toneParams, function (error, toneAnalysis) {
-		if (error) {
-			console.log(error);
-		} else {
-			let results = toneAnalysis.sentences_tone;
-			let document_tones = [];
-			let doc_tones = toneAnalysis.document_tone.tones;
-			for (let i = 0; i < doc_tones.length; i++) {
-				document_tones[i] = "['" + doc_tones[i].tone_name + "', " + doc_tones[i].score + "]";
-			}
-			let headlines_to_show = [];
-			for (let i = 0; i < headlines.length; i++) {
-				if (results[i].tones.length !== 0) {
-					console.log(headlines[i]);
-					console.log('-----------------------------');
-					headlines[i].tones = results[i].tones;
-					headlines_to_show.push(headlines[i]);
-				}
-			}
-			callback(headlines_to_show, document_tones);
+			let title = articles[i].description;
+			let text = title.replace(/\./g,'');
+			text = text.replace(/\-/g,'');
+			text = text.replace(/\>/g,'');
+			text = text.replace(/BuzzFeed News/g, '');
+			article_descriptions += text + '. ';
 		}
+
+		var toneParams = {
+				'tone_input': { 'text': article_descriptions },
+				'content_type': 'application/json',
+		};
+
+		toneAnalyzer.tone(toneParams, function (error, toneAnalysis) {
+			if (error) {
+				reject(error)
+			} else {
+				let results = toneAnalysis.sentences_tone;
+				let document_tones = [];
+				let doc_tones = toneAnalysis.document_tone.tones;
+				for (let i = 0; i < doc_tones.length; i++) {
+					document_tones[i] = "['" + doc_tones[i].tone_name + "', " + doc_tones[i].score + "]";
+				}
+				let articles_to_show = [];
+				for (let i = 0; i < results.length; i++) {
+					if (results[i].tones.length > 0) {
+						articles[i].tones = results[i].tones;
+						articles_to_show.push(articles[i]);
+					}
+				}
+				resolve(callback(articles_to_show, document_tones));
+			}
+		});
 	});
 }
 
 module.exports.getTopArticles = getTopArticles;
 module.exports.getAllArticles = getAllArticles;
-module.exports.buildHeadlines = buildHeadlines;
+module.exports.buildArticles = buildArticles;
 module.exports.getTones = getTones;
